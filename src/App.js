79fe4ee93e7ff9c31f1027523ffe39eccbe360c3
Sophia Mai <phuongmai001@gmail.com>
2018-08-24 12:09:22 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import ReactDOM from 'react-dom';
+
+class ContactList extends React.Component {
+  // class ContactList extends Component {...}
+  render() {
+    const people = [
+      {name: 'Michael'},
+      {name: 'Sophia'},
+      {name: 'Jack'}
+    ];
+
+    return <ol>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+      </ol>
+  }
+}
+
+/*
+ReactDOM.render(
+  <ContactList />,
+  document.getElementById('root')
+)
+*/
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
       </div>
     );
   }
