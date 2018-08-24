e943efaa1018b58802e596628cc1d69462d7c8e2
Sophia Mai <phuongmai001@gmail.com>
2018-08-24 14:22:40 -0700

    update list contacts view

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -7,8 +7,9 @@ class ListContacts extends Component {
       <ol className="contact-list">
         {this.props.contacts.map((contact) => (
           <li key={contact.id} className="contact-list-item">
-            <div className="contact-avatar" style={{ backgroundImage: `url(${contact.avatarURL})` }}/>
-            <img className="contact-avatar" src={contact.avatarURL} alt={contact.name} />
+            <div className="contact-avatar" style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}/>
             <div className="contact-details">
               <p>{contact.name}</p>
               <p>{contact.email}</p>
