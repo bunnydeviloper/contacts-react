eda35f7f760ffc9b823435de4d69e6391356e466
Sophia Mai <phuongmai001@gmail.com>
2018-08-24 14:41:05 -0700

    update state for component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import React, { Component } from 'react';
 
+// stateless functional component
+function ListContacts (props) {
+  return (
+    <ol className="contact-list">
+      {props.contacts.map((contact) => (
+        <li key={contact.id} className="contact-list-item">
+          <div className="contact-avatar" style={{
+            backgroundImage: `url(${contact.avatarURL})`
+          }}/>
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
+          <button className="contact-remove">
+            Remove
+          </button>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+/*
 class ListContacts extends Component {
-
   render() {
-    return (
-      <ol className="contact-list">
-        {this.props.contacts.map((contact) => (
-          <li key={contact.id} className="contact-list-item">
-            <div className="contact-avatar" style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}/>
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button className="contact-remove">
-              Remove
-            </button>
-          </li>
-        ))}
-      </ol>
-    );
+    return (<div>
+      {this.props.contacts}}
+      </div>
+    )
   }
 }
+*/
 
 export default ListContacts;
